cf08e7e0cc7f8ca6a3cdbda27f1ed77c9a4d94bc
Jarno <jarnovoogd@gmail.com>
2023-03-21 17:19:58 +0100

    COM : addition of comments

--- routes/followingrouter.js
+++ routes/followingrouter.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+/**----------------------
+ *    Jarno's router
+ *------------------------**/
 router.get('/', async (req, res) => {
     res.send("jarno's following router werkt!")
     console.log("jarno's following router werkt!")
