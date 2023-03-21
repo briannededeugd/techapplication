b4fd50f0e2df9449ba4c876f35c89e175b04bf2c
Jarno <jarnovoogd@gmail.com>
2023-03-21 14:19:41 +0100

    FEAT : succesfully required and implemented the different routers

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const port = process.env.PORT || 3000
 
 require("dotenv").config();
 
-const followingRouter = require('./routes/followingrouter');
-const registerRouter = require('./routes/registerrouter');
+const followingRouter = require('./routes/followingRouter');
+const registerRouter = require('./routes/registerRouter');
+const brMatchingRouter = require('./routes/brMatchingRouter');
+const elMatchingRouter = require('./routes/elMatchingRouter');
+const likingRouter = require('./routes/likingRouter');
 
 
 
 app.use('/following', followingRouter);
+
+app.use('/register', registerRouter);
+
+app.use('/brmatching', brMatchingRouter);
+
+app.use('/elmatching', elMatchingRouter);
+
+app.use('/liking', likingRouter)
+
 
 app.listen(port, () => {
     console.log(`Server is listening to port: ${port}`);
