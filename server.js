59b160c126c1d11bf9cc9b12c4d8fa9f890c8eb1
Jarno <jarnovoogd@gmail.com>
2023-03-21 14:44:23 +0100

    COM : addition of comments in server.js

--- server.js
+++ server.js
@@ -12,16 +12,57 @@ const likingRouter = require('./routes/likingRouter');
 
 
 
+
+/**========================================================================
+ *                           Routing
+ *========================================================================**/ 
+
+/**----------------------
+ *    Home Page
+ *------------------------**/
+app.get('/', (req, res) => {
+    res.send('Welkom op de homepagina')
+})
+
+/**----------------------
+ *    Jarno's Following
+ *------------------------**/
 app.use('/following', followingRouter);
 
+/**----------------------
+ *    Tristan's Register
+ *------------------------**/
 app.use('/register', registerRouter);
 
+/**----------------------
+ *    Brianne's Matching
+ *------------------------**/
 app.use('/brmatching', brMatchingRouter);
 
+/**----------------------
+ *    Elaine's Matching
+ *------------------------**/
 app.use('/elmatching', elMatchingRouter);
 
+/**----------------------
+ *    Bryan's Liking
+ *------------------------**/
 app.use('/liking', likingRouter)
 
+
+
+/**========================================================================
+ *                           404 Error Handler
+ *========================================================================**/ 
+
+app.use((req, res) => {
+    res.status(404).send("We're sorry, we were not able to find the page you were looking for");
+});
+
+
+/**========================================================================
+ *                           Start Webserver
+ *========================================================================**/ 
 
 app.listen(port, () => {
     console.log(`Server is listening to port: ${port}`);
