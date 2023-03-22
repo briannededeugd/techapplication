7e04721473a5774cf7af364724bd52af5687be43
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-03-22 19:15:05 +0100

    COM : routes won't work yet, ask for help tmrw

--- routes/matchingRouter.js
+++ routes/matchingRouter.js
@@ -1,14 +1,60 @@
-const express = require("express");
+//! HOE WERKT NAVIGATIE? LUKT NIET. HANDMATIG localhost:3000/matching/matchinglanguages WERKT WEL, ANDERE MANIEREN NIET 
+
+/* eslint-disable no-undef */
+
+const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 /**========================================================================
  *                        BRIANNE'S MATCHING ROUTER
  *========================================================================**/
-router.get("/", async (req, res) => {
-	res.send("brianne's matching router werkt!");
-	console.log("brianne's matching router werkt!");
+router.get('/', async (req, res) => {
+	console.log('briannes matching router werkt!');
+	res.render('pages/matchingfeels');
 });
 
-// TEST
+router.get('/matchingelements', onElements);
+
+function onElements(req, res) {
+	res.render('pages/matchingelements');
+}
+
+router.get('/matchinglanguage', onLanguage);
+
+function onLanguage(req, res){
+	res.render('pages/matchinglanguage');
+}
+
+/**========================================================================
+ *                           DATA VERWERKEN ETC
+ *========================================================================**/
+
+let selectedFeatures;
+let selectedMoods;
+let selectedLanguage;
+
+router.post('/userPost', handleUserPost);
+
+function handleUserPost(req, res) {
+	const formData = req.body;
+	const nextPage = formData['nextPage'];
+	res.redirect(nextPage);
+
+	if (selectedFeatures === undefined) {
+		selectedFeatures = req.body.feature;
+	}
+
+	if (selectedMoods === undefined) {
+		selectedMoods = req.body.moods;
+	}
+
+	if (selectedLanguage === undefined) {
+		selectedLanguage = req.body.language;
+	}
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+/* eslint-disable no-undef */
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +20,7 @@ const likingRouter = require('./routes/likingRouter');
  *------------------------**/
 const { songs } = require('./routes/songSchema');
 const { users } = require('./routes/userSchema');
-console.log("🚀 ~ file: server.js:17 ~ users:", users)
+console.log('🚀 ~ file: server.js:17 ~ users:', users);
 
 /**----------------------
  *    Defining and connecting to database
@@ -26,50 +28,49 @@ console.log("🚀 ~ file: server.js:17 ~ users:", users)
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}${process.env.DB_URI}`;
 
 async function main() {
-    await mongoose.connect(uri,{
-      dbName: process.env.DB_NAME,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Succesfully connected")
+	await mongoose.connect(uri, {
+		dbName: process.env.DB_NAME,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	console.log('Succesfully connected');
 }
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 /**========================================================================
  *                           Middleware
- *========================================================================**/ 
+ *========================================================================**/
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 /**========================================================================
  *                           Templating
- *========================================================================**/ 
+ *========================================================================**/
 
 app.set('view engine', 'ejs');
 
-
 /**========================================================================
  *                           Routing
- *========================================================================**/ 
+ *========================================================================**/
 
 /**----------------------
  *    Home Page
  *------------------------**/
 app.get('/', async (req, res) => {
-    res.send('Welkom op de homepagina');
+	res.send('Welkom op de homepagina');
 
-    try {
-        const allSongs = await songs.find({});
-        // console.log("🚀 ~ file: server.js:58 ~ app.get ~ allSongs:", allSongs);
+	try {
+		const allSongs = await songs.find({});
+		console.log('🚀 ~ file: server.js:66 ~ app.get ~ allSongs:', allSongs);
+		
 
-        const allUsers = await users.find({});
-        // console.log("🚀 ~ file: server.js:61 ~ app.get ~ allUsers:", allUsers)
-        
-    } catch (error) {
-        console.error(error);
-    }
+		const allUsers = await users.find({});
+		console.log('🚀 ~ file: server.js:70 ~ app.get ~ allUsers:', allUsers);
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 /**----------------------
@@ -97,24 +98,22 @@ app.use('/filter', filterRouter);
  *------------------------**/
 app.use('/liking', likingRouter);
 
-
-
 /**========================================================================
  *                           404 Error Handler
- *========================================================================**/ 
+ *========================================================================**/
 
 app.use((req, res) => {
-    res.status(404).send("We're sorry, we were not able to find the page you were looking for");
+	res
+		.status(404)
+		.send(
+			'We`re sorry, we were not able to find the page you were looking for'
+		);
 });
-
 
 /**========================================================================
  *                           Start Webserver
- *========================================================================**/ 
+ *========================================================================**/
 
 app.listen(port, () => {
-    console.log(`Server is listening to port: ${port}`);
+	console.log(`Server is listening to port: ${port}`);
 });
-
-
-
