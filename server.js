2f8b5900272be80c14b91947a8434205acb310b1
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-03-23 23:09:05 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 /**========================================================================
  *                           Requiring the seperate routes
@@ -15,15 +14,13 @@ const matchingRouter = require('./routes/matchingRouter');
 const filterRouter = require('./routes/filterRouter');
 const likingRouter = require('./routes/likingRouter');
 
-
 /**========================================================================
  *                           Requiring the mongoose schemas
  *========================================================================**/
 
 const { songs } = require('./routes/songSchema');
-const { users } = require('./routes/userSchema');
+const { User } = require('./routes/userSchema');
 const { admin } = require('./routes/adminSchema');
-
 
 /**========================================================================
  *                           Defining and connecting to database
@@ -31,31 +28,29 @@ const { admin } = require('./routes/adminSchema');
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}${process.env.DB_URI}`;
 
-async function main() {
-    await mongoose.connect(uri,{
+async function main () {
+    await mongoose.connect(uri, {
       dbName: process.env.DB_NAME,
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log("Succesfully connected");
+    console.log('Succesfully connected');
 }
 main().catch(err => console.log(err));
-
 
 /**========================================================================
  *                           Middleware
  *========================================================================**/
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 /**========================================================================
  *                           Templating
  *========================================================================**/
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 /**========================================================================
  *                           Routing
@@ -64,62 +59,54 @@ app.set("view engine", "ejs");
 /**----------------------
  *    Home Page
  *------------------------**/
-app.get("/", async (req, res) => {
-  res.send("Welkom op de homepagina");
+app.get('/', async (req, res) => {
+  res.send('Welkom op de homepagina');
 
   try {
     const allSongs = await songs.find({});
     // console.log("🚀 ~ file: server.js:58 ~ app.get ~ allSongs:", allSongs);
 
-<<<<<<< HEAD
-    const allUsers = await user.find({});
-    // console.log("🚀 ~ file: server.js:61 ~ app.get ~ allUsers:", allUsers)
-  } catch (error) {
-    console.error(error);
-  }
-=======
-        const allUsers = await users.find({});
+        const allUsers = await User.find({});
         // console.log("🚀 ~ file: server.js:61 ~ app.get ~ allUsers:", allUsers)
-        
+
         const allAdmins = await admin.find({});
         // console.log("🚀 ~ file: server.js:73 ~ app.get ~ allAdmins:", allAdmins)
     } catch (error) {
         console.error(error);
     }
->>>>>>> main
 });
 
 /**----------------------
  *    Jarno's Following
  *------------------------**/
-app.use("/following", followingRouter);
+app.use('/following', followingRouter);
 
 /**----------------------
  *    Tristan's Register
  *------------------------**/
-app.use("/register", registerRouter);
+app.use('/tristan', registerRouter);
 
 /**----------------------
  *    Brianne's Matching
  *------------------------**/
-app.use("/matching", matchingRouter);
+app.use('/matching', matchingRouter);
 
 /**----------------------
  *    Elaine's Matching
  *------------------------**/
-app.use("/filter", filterRouter);
+app.use('/filter', filterRouter);
 
 /**----------------------
  *    Bryan's Liking
  *------------------------**/
-app.use("/liking", likingRouter);
+app.use('/liking', likingRouter);
 
 /**========================================================================
  *                           404 Error Handler
  *========================================================================**/
 
 app.use((req, res) => {
-  res.status(404).send("We're sorry, we were not able to find the page you were looking for");
+  res.status(404).send('We\'re sorry, we were not able to find the page you were looking for');
 });
 
 /**========================================================================
