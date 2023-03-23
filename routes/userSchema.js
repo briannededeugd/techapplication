ae02ef2eec4e97c0b0460d9dd6cc14d0b5b0d6be
Jarno <jarnovoogd@gmail.com>
2023-03-23 14:02:00 +0100

    FEAT : additions to the user mongoose schema

--- routes/userSchema.js
+++ routes/userSchema.js
@@ -1,19 +1,43 @@
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
+const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	_id: ObjectId,
-	firstName: String,
-	lastName: String,
-	age: String,
-	email: String,
-	profilePicture: String,
-	mood: Array,
-	favouriteArtist: Array,
-	favouriteSongs: Array,
-	follow : Boolean,
+  _id: ObjectId,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  profilePicture: {
+    type: String,
+    required: false
+  },
+  mood: Array,
+  favouriteArtist: Array,
+  favouriteSongs: Array,
+  follow: Boolean,
+  password: {
+    type: String,
+    required: true
+  },
+  password2: {
+    type: String,
+    required: true
+  }
 });
 
-const users = mongoose.model('users', userSchema);
+const users = mongoose.model("users", userSchema);
 
 module.exports = { users };
