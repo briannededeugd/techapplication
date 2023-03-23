4d65926130be9f3236a9678fb64b20a1f6e49d99
lainecedes <elaine.parieh@gmail.com>
2023-03-23 13:31:18 +0100

    ADD: code changes with eslint rules

--- routes/filterRouter.js
+++ routes/filterRouter.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const router = express.Router()
+/* eslint-disable no-undef */
+// die no-undef errors zijn irritant dus het staat uit
+
+const express = require('express');
+const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("elaine's filter router werkt!")
-    console.log("elaine's filter router werkt!")
-})
+    res.send('elaines filter router werkt!');
+    console.log('elaines filter router werkt!');
+});
 
 
 module.exports = router;
