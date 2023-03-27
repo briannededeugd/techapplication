2fdd230303ad3e08856cc08fcbcac27bf2ae5393
lainecedes <elaine.parieh@gmail.com>
2023-03-27 17:51:02 +0200

    FEAT: add filtering functionality and ux enhancement

--- routes/filterRouter.js
+++ routes/filterRouter.js
@@ -1,19 +1,36 @@
 /* eslint-disable no-undef */
-// die no-undef errors zijn irritant dus het staat uit
+// no-undef errors are annoying so turned it off
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { users } = require('./userSchema');
+const { users } = require("./userSchema");
 
-router.get('/', async (req, res) => {
-    // const allUsers = users.find({mood});
-
-    res.render('pages/filterpage');
-    console.log('elaines filter router werkt!');
+router.get("/", async (req, res) => {
+  const allUsers = await users.find({});
+  console.log("elaines filter router werkt!");
+  res.render("pages/filterpage", { data: allUsers });
 });
 
-console.log(users);
+router.post("/filterresults", async (req, res) => {
+  // Search for both moods or favourite artists in the database and render in view
+  const filteredUsers = await users.find({
+    $or: [
+      { mood: req.body.mood },
+      { favouriteArtists: req.body.favouriteArtists },
+    ],
+  });
 
+  // Show the user the options they selected
+  const filterOptions = [req.body.mood, req.body.favouriteArtists];
+
+  console.log(filteredUsers);
+  console.log(filterOptions);
+
+  res.render("pages/filterresults", {
+    data: filteredUsers,
+    filters: filterOptions,
+  });
+});
 
 module.exports = router;
