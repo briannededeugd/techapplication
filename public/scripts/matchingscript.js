3d9376103ebdd0b16ccbbf2912b289346c817f8f
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-03-31 17:27:34 +0200

    FEAT : view 'almost-matches' and play one by one

--- public/scripts/matchingscript.js
+++ public/scripts/matchingscript.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-undef */
 console.log('js connected!!');
 
-// Play knop wordt pauzeknop als erop wordt geklikt
+/**========================================================================
+ *                           MAIN SONG / BEST MATCH
+ *========================================================================**/
+
+// Play button becomes pause button when clicked
 const playButton = document.getElementById('play-knop');
 const audio = document.getElementById('current-audio');
 
 if (playButton) {
-	// Er wordt eerst gekeken of er een playknop bestaat op de pagina, zodat er geen error komt
+	// First step is checking if there's a play button on the page to avoid any errors
 	playButton.addEventListener('click', () => {
 		console.log('Knop geklikt');
 
@@ -17,6 +21,14 @@ if (playButton) {
 			playButton.textContent = 'play_arrow';
 			audio.pause();
 		}
+	});
+
+	audio.addEventListener('play', () => {
+		playButton.textContent = 'pause';
+	});
+
+	audio.addEventListener('pause', () => {
+		playButton.textContent = 'play_arrow';
 	});
 }
 
@@ -51,3 +63,142 @@ if (audio) {
 		}
 	});
 }
+
+/**========================================================================
+ *                         SECOND BEST MATCH
+ *========================================================================**/
+// Play knop wordt pauzeknop als erop wordt geklikt
+const playTweedeMatch = document.getElementById('play2nd');
+const tweedeAudio = document.getElementById('secondaudio');
+
+if (playTweedeMatch) {
+	// Er wordt eerst gekeken of er een playknop bestaat op de pagina, zodat er geen error komt
+	playTweedeMatch.addEventListener('click', () => {
+		console.log('Knop geklikt');
+
+		if (playTweedeMatch.textContent === 'play_arrow') {
+			playTweedeMatch.textContent = 'pause';
+			tweedeAudio.play();
+		} else {
+			playTweedeMatch.textContent = 'play_arrow';
+			tweedeAudio.pause();
+			tweedeAudio.currentTime = 0;
+		}
+	});
+
+	tweedeAudio.addEventListener('play', () => {
+		playTweedeMatch.textContent = 'pause';
+	});
+
+	tweedeAudio.addEventListener('pause', () => {
+		playTweedeMatch.textContent = 'play_arrow';
+	});
+}
+
+/**========================================================================
+ *                         THIRD BEST MATCH
+ *========================================================================**/
+// Play knop wordt pauzeknop als erop wordt geklikt
+const playDerdeMatch = document.getElementById('play3rd');
+const derdeAudio = document.getElementById('thirdaudio');
+
+if (playDerdeMatch) {
+	// Er wordt eerst gekeken of er een playknop bestaat op de pagina, zodat er geen error komt
+	playDerdeMatch.addEventListener('click', () => {
+		console.log('Knop geklikt');
+
+		if (playDerdeMatch.textContent === 'play_arrow') {
+			playDerdeMatch.textContent = 'pause';
+			derdeAudio.play();
+		} else {
+			playDerdeMatch.textContent = 'play_arrow';
+			derdeAudio.pause();
+			derdeAudio.currentTime = 0;
+		}
+	});
+
+	derdeAudio.addEventListener('play', () => {
+		playDerdeMatch.textContent = 'pause';
+	});
+
+	derdeAudio.addEventListener('pause', () => {
+		playDerdeMatch.textContent = 'play_arrow';
+	});
+}
+
+/**========================================================================
+ *                         FOURTH BEST MATCH
+ *========================================================================**/
+// Play knop wordt pauzeknop als erop wordt geklikt
+const playVierdeMatch = document.getElementById('play4th');
+const vierdeAudio = document.getElementById('fourthaudio');
+
+if (playVierdeMatch) {
+	// Er wordt eerst gekeken of er een playknop bestaat op de pagina, zodat er geen error komt
+	playVierdeMatch.addEventListener('click', () => {
+		console.log('Knop geklikt');
+
+		if (playVierdeMatch.textContent === 'play_arrow') {
+			playVierdeMatch.textContent = 'pause';
+			vierdeAudio.play();
+		} else {
+			playVierdeMatch.textContent = 'play_arrow';
+			vierdeAudio.pause();
+			vierdeAudio.currentTime = 0;
+		}
+	});
+
+	vierdeAudio.addEventListener('play', () => {
+		playVierdeMatch.textContent = 'pause';
+	});
+
+	vierdeAudio.addEventListener('pause', () => {
+		playVierdeMatch.textContent = 'play_arrow';
+	});
+}
+
+/**========================================================================
+ *                         FIFTH BEST MATCH
+ *========================================================================**/
+const playVijfdeMatch = document.getElementById('play5th');
+const vijfdeAudio = document.getElementById('fifthaudio');
+
+if (playVijfdeMatch) {
+	playVijfdeMatch.addEventListener('click', () => {
+		console.log('Knop geklikt');
+
+		if (playVijfdeMatch.textContent === 'play_arrow') {
+			playVijfdeMatch.textContent = 'pause';
+			vijfdeAudio.play();
+		} else {
+			playVijfdeMatch.textContent = 'play_arrow';
+			vijfdeAudio.pause();
+			vijfdeAudio.currentTime = 0;
+		}
+	});
+
+	vijfdeAudio.addEventListener('play', () => {
+		playVijfdeMatch.textContent = 'pause';
+	});
+
+	vijfdeAudio.addEventListener('pause', () => {
+		playVijfdeMatch.textContent = 'play_arrow';
+	});
+}
+
+// Initialize variables to keep track of currently playing audio
+let currentlyPlaying = null;
+
+// Get all the audio elements on the page
+const audios = document.querySelectorAll('audio');
+
+// Add event listeners to each audio element
+audios.forEach((audio) => {
+	audio.addEventListener('play', () => {
+		// Pause the currently playing audio, if any is playing
+		if (currentlyPlaying && currentlyPlaying !== audio) {
+			currentlyPlaying.pause();
+		}
+		currentlyPlaying = audio;
+	});
+});
