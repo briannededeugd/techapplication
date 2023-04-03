53cd14415f0a44897446f5e5e5ca9ddae83c5ef4
lainecedes <elaine.parieh@gmail.com>
2023-04-03 17:30:02 +0200

    ADD: changes to routes and controller

--- routes/filterRouter.js
+++ routes/filterRouter.js
@@ -1,36 +1,28 @@
 /* eslint-disable no-undef */
-// no-undef errors are annoying so turned it off
 
 const express = require("express");
 const router = express.Router();
 
+/**========================================================================
+ *                             Mongoose Schema
+ *========================================================================**/
+
 const { users } = require("./userSchema");
 
-router.get("/", async (req, res) => {
-  const allUsers = await users.find({});
-  console.log("elaines filter router werkt!");
-  res.render("pages/filterpage", { data: allUsers });
-});
 
-router.post("/filterresults", async (req, res) => {
-  // Search for both moods or favourite artists in the database and render in view
-  const filteredUsers = await users.find({
-    $or: [
-      { mood: req.body.mood },
-      { favouriteArtists: req.body.favouriteArtists },
-    ],
-  });
+/**========================================================================
+ *                             Controllers
+ *========================================================================**/
 
-  // Show the user the options they selected
-  const filterOptions = [req.body.mood, req.body.favouriteArtists];
+const filteringController = require('../controllers/filteringController');
 
-  console.log(filteredUsers);
-  console.log(filterOptions);
+/**========================================================================
+ *                        Filter page and results
+ *========================================================================**/
 
-  res.render("pages/filterresults", {
-    data: filteredUsers,
-    filters: filterOptions,
-  });
-});
+router.get('/', filteringController.filterOnPage);
+
+router.post('/filterresults', filteringController.getFilterResults);
+
 
 module.exports = router;
